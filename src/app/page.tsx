c32df9059684ddf3007fa3eef148c6eae8de813c
junyi <junyi@aeonbank.com.my>
2025-04-11 13:29:29 +0800

    added most content in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,150 @@
 import Image from "next/image";
+import Link from "next/link";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
+const faq: { title: string; description: string }[] = [
+  {
+    title: 'Do you provide full-time or part-time domestic helper/maid?', 
+    description: 'We will provide full-time maids who live with the employers. However, part-time/temporary / live out maids are only available on case by case basis.'
+  },
+  {
+    title: 'Where do the maid come from? And what are their ethnics and religions?', 
+    description: 'The maids from our agency are mainly Indonesian of Central and East Java, Lombok and Nusa Tenggara origins. They are Muslims, Christians or Hindus being the three main religions in Indonesia.'
+  },
+  {
+    title: 'What are their ages?', 
+    description: 'In accordance to the immigration law, all domestic helpers must be between 21 to 45 years of age for the first year hiring.'
+  },
+  {
+    title: 'How long is the maid employment contract?', 
+    description: 'The maid employment contract is binding for a period of two years upon which the maid shall be returned to the home country for a short leave. The contract can be renewed if the maid agreed to return to the same employer otherwise the employer is advised to secure the services of a different maid.'
+  },
+]
+
+const feedbacks: { author: string; position: string; content: string; }[] = [
+  {
+    author: 'John Doe',
+    position: 'IT Manager at Google',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ipsa perspiciatis excepturi. Molestiae facere tempore numquam ullam veritatis omnis voluptate. Possimus laudantium, eius nisi officia facilis amet qui deserunt? Odit!'
+  },
+  {
+    author: 'John Doe',
+    position: 'IT Manager at Google',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ipsa perspiciatis excepturi. Molestiae facere tempore numquam ullam veritatis omnis voluptate. Possimus laudantium, eius nisi officia facilis amet qui deserunt? Odit!'
+  },
+  {
+    author: 'John Doe',
+    position: 'IT Manager at Google',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ipsa perspiciatis excepturi. Molestiae facere tempore numquam ullam veritatis omnis voluptate. Possimus laudantium, eius nisi officia facilis amet qui deserunt? Odit!'
+  },
+  {
+    author: 'John Doe',
+    position: 'IT Manager at Google',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ipsa perspiciatis excepturi. Molestiae facere tempore numquam ullam veritatis omnis voluptate. Possimus laudantium, eius nisi officia facilis amet qui deserunt? Odit!'
+  },
+]
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      {/* Cover */}
+      <div className="grid grid-cols-2 h-[70vh] bg-amber-50 px-32">
+        <div></div>
+        <div className="my-auto flex flex-col gap-5">
+          <span className="mb-12">
+            Featured Modelling  <a className="hover:text-amber-800 ease-in-out duration-150" href='https://www.domesiconcepthouse.cz/en/' target="_blank">
+              domesiconcepthouse
+            </a>
+          </span>
+          <h1 className="text-6xl tracking-tight font-bold">
+            The Scent of Wood
+          </h1>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ipsa perspiciatis excepturi. Molestiae facere tempore numquam ullam veritatis omnis voluptate. Possimus laudantium, eius nisi officia facilis amet qui deserunt? Odit!
+          </span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      {/* About Us */}
+      <div className="flex flex-col gap-5 text-center p-32">
+        <h1 className="text-3xl tracking-tight font-bold">
+          About Us
+        </h1>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ipsa perspiciatis excepturi. Molestiae facere tempore numquam ullam veritatis omnis voluptate. Possimus laudantium, eius nisi officia facilis amet qui deserunt? Odit!
+        </span>
+      </div>
+      {/* Services */}
+      <div className="p-32">
+        <div className="grid grid-cols-6 grid-rows-6 gap-8">
+          <div className="row-span-4 col-span-2 bg-amber-50">Image</div>
+          <div className="row-span-3 col-span-4 flex flex-col gap-2 items-start py-auto">
+            <h1 className="text-3xl tracking-tight font-bold">
+              Our Services
+            </h1>
+            <span>Expert designers are here to offer to remodel and organize the living environment of clients based on the needs of the clients.</span>
+            <Button asChild variant="outline"><Link href='/'>Explore Remodeling</Link></Button>
+          </div>
+          <div className="row-span-2 col-span-4 flex flex-col gap-2 col-start-1 items-end py-auto">
+            <span>Increasing happiness by providing professional full-time maid, part-time maid and organizing helper/expert.</span>
+            <Button asChild variant="outline"><Link href='/'>Explore Housekeeping & Organization</Link></Button>
+          </div>
+          <div className="row-span-3 col-span-2 col-start-5 row-start-4 bg-blue-50">Image</div>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 p-32 bg-amber-50">
+        <h1 className="text-3xl tracking-tight font-bold mt-8">
+          Frequently Asked Questions
+        </h1>
+        <Accordion type="single" collapsible className="w-full">
+          {faq.map((elem) => (
+            <AccordionItem key={elem.title} value={elem.title}>
+              <AccordionTrigger className="text-lg tracking-tight font-medium">{elem.title}</AccordionTrigger>
+              <AccordionContent>{elem.description}</AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+      <div className="flex flex-col gap-5 text-center p-32">
+        <h1 className="text-3xl tracking-tight font-bold">
+          Customer Reviews
+        </h1>
+        <span>
+          These are our 5-Star reviews from our customers.
+        </span>
+        <Carousel className="w-full mt-8">
+          <CarouselContent className="w-1/3">
+            {feedbacks.map((elem, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex flex-col aspect-2/1 text-justify p-6">
+                      <span className="text-sm text-slate-700 mb-5">{elem.content}</span>
+                      <h1 className="text-lg font-bold">{elem.author}</h1>
+                      <span className="font-semibold">{elem.position}</span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+    </>
   );
 }
